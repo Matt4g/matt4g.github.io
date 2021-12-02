761234f297bf1aef17fd0b81919acc3d8fe34644
Matt Gladura <75256340+Matt4g@users.noreply.github.com>
2021-12-02 14:01:44 -0500

    f

---
+++ public/quiz.js
@@ -0,0 +1,26 @@
+
+let q2 = document.getElementsByName('q2');
+let q3 = document.getElementsByName('q3');
+let q4 = document.getElementsByName('q4');
+let q5 = document.getElementsByName('q5');
+
+function submitAnswer() {
+
+    let q1 = document.getElementsByName('q1');
+    var val1= "";
+    for (var i = 0, length = q1.length; i < length; i++) {
+        if (q1[i].checked) {
+           val1 = q1[i].value; 
+           break;
+         }
+    }
+    
+    if (val1 == "" ) {
+     document.getElementById('ta1').innerText = 'please select choice answer';
+    } else if ( val == "B. Bridge 4" ) {
+      'Answer is correct !'
+    } else
+     {
+      alert('Answer is wrong');
+    }
+  };
